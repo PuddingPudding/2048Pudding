85ec6824651d2466ead57f8b51c1dad82c2ecf30
PuddingPudding <hwsh70119@gmail.com>
2018-02-02 14:27:24 +0800

    已加入 "可否推" 的判斷，也加入了計分欄位跟 "Game Over!" 的字樣，基本上已完成

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,14 +10,19 @@ public class GameManager : MonoBehaviour
     public Vector2 v2BlockInterval = new Vector2(60, 60); //每個格子物件間的間隔
     public Canvas cUICanvas;
     public Transform tInitPosition; //第一個生成格子物件的位子
+    public Text textScore;
+    public GameObject gameOverView;//失敗時會看到的東西
 
     private List<int> iListCheckBoard; //用來紀錄整個2048棋盤中每一格數字的串列
-    private List<BlockScript> listBlockScript;
+    private List<BlockScript> listBlockScript;//用來存取每一個格子的程式碼，格子身上的程式碼可以讓你設定顯示的數字
+    private int iScore = 0;
     // Use this for initialization
     void Start()
     {
         InitGame();
         UpdateGame();
+        iScore = 0;
+        gameOverView.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,25 +30,41 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            this.PushLeft();
-            this.SpawnBlock(1);
+            if (this.PushAbleLeft())
+            {
+                this.PushLeft();
+                this.SpawnBlock(1);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            this.PushRight();
-            this.SpawnBlock(1);
+            if (this.PushAbleRight())
+            {
+                this.PushRight();
+                this.SpawnBlock(1);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            this.PushUp();
-            this.SpawnBlock(1);
+            if (this.PushAbleUp())
+            {
+                this.PushUp();
+                this.SpawnBlock(1);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            this.PushDown();
-            this.SpawnBlock(1);
+            if (this.PushAbleDown())
+            {
+                this.PushDown();
+                this.SpawnBlock(1);
+            }
         }
         UpdateGame();
+        if(!PushAbleDown() && !PushAbleLeft() && !PushAbleRight() && !PushAbleUp() )
+        {
+            this.gameOverView.SetActive(true);
+        }
     }
 
     public void InitGame()
@@ -62,7 +84,6 @@ public class GameManager : MonoBehaviour
             BlockScript BSTemp = blockTemp.GetComponent<BlockScript>();
             listBlockScript.Add(BSTemp);
         }
-        Debug.Log(iListCheckBoard.Count);
         SpawnBlock(2);
     }
 
@@ -97,6 +118,7 @@ public class GameManager : MonoBehaviour
         {
             listBlockScript[i].SetText(iListCheckBoard[i]);
         }
+        textScore.text = "score: " + this.iScore;
     }
 
     public void PushLeft()
@@ -129,9 +151,10 @@ public class GameManager : MonoBehaviour
                             if (iListCheckBoard[iComparing] == iListCheckBoard[iPolling])//若比對者與輪尋者皆同，兩者合併
                             {
                                 iListCheckBoard[iComparing] += iListCheckBoard[iPolling];
+                                this.iScore += iListCheckBoard[iComparing];
                                 iListCheckBoard[iPolling] = 0;
                             }
-                            else if(iListCheckBoard[iComparing+1] == 0)//若比對者的隔壁沒東西(0)，那就把輪尋到的那個數字直接搬過來
+                            else if (iListCheckBoard[iComparing + 1] == 0)//若比對者的隔壁沒東西(0)，那就把輪尋到的那個數字直接搬過來
                             {
                                 iListCheckBoard[iComparing + 1] += iListCheckBoard[iPolling];
                                 iListCheckBoard[iPolling] = 0;
@@ -148,6 +171,31 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public bool PushAbleLeft()
+    {
+        bool pushAble = false; //是否能推
+        int iChecking;//檢查用的格子，從最開始(第0個)檢查到最後
+        for (int i = 0; i < iLength && !pushAble; i++)
+        {
+            for (int j = 0; j < iLength - 1 && !pushAble; j++)
+            {
+                iChecking = (i * iLength) + j;
+                if (iListCheckBoard[iChecking] == 0)
+                {
+                    if (iListCheckBoard[iChecking + 1] != 0)
+                    {
+                        pushAble = true;
+                    }
+                }
+                else if (iListCheckBoard[iChecking] == iListCheckBoard[iChecking + 1])
+                {
+                    pushAble = true;
+                }
+            }
+        }
+        return pushAble;
+    }
+
     public void PushRight()
     {
         int iComparing; //正在受比對的格子索引值
@@ -158,11 +206,11 @@ public class GameManager : MonoBehaviour
             for (int j = 0; j < iLength; j++)
             {
                 pollFinish = false;
-                iComparing = ( (i+1) * iLength -1) - j;
+                iComparing = ((i + 1) * iLength - 1) - j;
                 iPolling = iComparing - 1;
                 while (!pollFinish)
                 {
-                    if (iPolling < (i * iLength) )//若輪尋格子超出了目前的這條行線
+                    if (iPolling < (i * iLength))//若輪尋格子超出了目前的這條行線
                     {
                         pollFinish = true;
                     }
@@ -178,6 +226,7 @@ public class GameManager : MonoBehaviour
                             if (iListCheckBoard[iComparing] == iListCheckBoard[iPolling])//若比對者與輪尋者皆同，兩者合併
                             {
                                 iListCheckBoard[iComparing] += iListCheckBoard[iPolling];
+                                this.iScore += iListCheckBoard[iComparing];
                                 iListCheckBoard[iPolling] = 0;
                             }
                             else if (iListCheckBoard[iComparing - 1] == 0)//若比對者的隔壁沒東西(0)，那就把輪尋到的那個數字直接搬過來
@@ -197,6 +246,31 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public bool PushAbleRight()
+    {
+        bool pushAble = false; //是否能推
+        int iChecking;//檢查用的格子，從最開始(第0個)檢查到最後
+        for (int i = 0; i < iLength && !pushAble; i++)
+        {
+            for (int j = 0; j < iLength - 1 && !pushAble; j++)
+            {
+                iChecking = ((i + 1) * iLength - 1) - j;
+                if (iListCheckBoard[iChecking] == 0)
+                {
+                    if (iListCheckBoard[iChecking - 1] != 0)
+                    {
+                        pushAble = true;
+                    }
+                }
+                else if (iListCheckBoard[iChecking] == iListCheckBoard[iChecking - 1])
+                {
+                    pushAble = true;
+                }
+            }
+        }
+        return pushAble;
+    }
+
     public void PushUp()
     {
         int iComparing; //正在受比對的格子索引值
@@ -207,11 +281,11 @@ public class GameManager : MonoBehaviour
             for (int j = 0; j < iLength; j++)
             {
                 pollFinish = false;
-                iComparing = i + j*iLength;
+                iComparing = i + (j * iLength);
                 iPolling = iComparing + iLength;
                 while (!pollFinish)
                 {
-                    if (iPolling >= iLength*iLength)//若輪尋格子超出了目前的這條行線
+                    if (iPolling >= iLength * iLength)//若輪尋格子超出了目前的這條行線
                     {
                         pollFinish = true;
                     }
@@ -227,6 +301,7 @@ public class GameManager : MonoBehaviour
                             if (iListCheckBoard[iComparing] == iListCheckBoard[iPolling])//若比對者與輪尋者皆同，兩者合併
                             {
                                 iListCheckBoard[iComparing] += iListCheckBoard[iPolling];
+                                this.iScore += iListCheckBoard[iComparing];
                                 iListCheckBoard[iPolling] = 0;
                             }
                             else if (iListCheckBoard[iComparing + iLength] == 0)//若比對者的隔壁沒東西(0)，那就把輪尋到的那個數字直接搬過來
@@ -246,6 +321,31 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public bool PushAbleUp()
+    {
+        bool pushAble = false; //是否能推
+        int iChecking;//檢查用的格子，從最開始(第0個)檢查到最後
+        for (int i = 0; i < iLength && !pushAble; i++)
+        {
+            for (int j = 0; j < iLength - 1 && !pushAble; j++)
+            {
+                iChecking = i + (j * iLength);
+                if (iListCheckBoard[iChecking] == 0)
+                {
+                    if (iListCheckBoard[iChecking + iLength] != 0)
+                    {
+                        pushAble = true;
+                    }
+                }
+                else if (iListCheckBoard[iChecking] == iListCheckBoard[iChecking + iLength])
+                {
+                    pushAble = true;
+                }
+            }
+        }
+        return pushAble;
+    }
+
     public void PushDown()
     {
         int iComparing; //正在受比對的格子索引值
@@ -256,7 +356,7 @@ public class GameManager : MonoBehaviour
             for (int j = 0; j < iLength; j++)
             {
                 pollFinish = false;
-                iComparing = i +( (iLength-1) - j) * iLength;
+                iComparing = i + ((iLength - 1) - j) * iLength;
                 iPolling = iComparing - iLength;
                 while (!pollFinish)
                 {
@@ -276,6 +376,7 @@ public class GameManager : MonoBehaviour
                             if (iListCheckBoard[iComparing] == iListCheckBoard[iPolling])//若比對者與輪尋者皆同，兩者合併
                             {
                                 iListCheckBoard[iComparing] += iListCheckBoard[iPolling];
+                                this.iScore += iListCheckBoard[iComparing];
                                 iListCheckBoard[iPolling] = 0;
                             }
                             else if (iListCheckBoard[iComparing - iLength] == 0)//若比對者的隔壁沒東西(0)，那就把輪尋到的那個數字直接搬過來
@@ -293,6 +394,31 @@ public class GameManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    public bool PushAbleDown()
+    {
+        bool pushAble = false; //是否能推
+        int iChecking;//檢查用的格子，從最開始(第0個)檢查到最後
+        for (int i = 0; i < iLength && !pushAble; i++)
+        {
+            for (int j = 0; j < iLength - 1 && !pushAble; j++)
+            {
+                iChecking = i + ((iLength - 1) - j) * iLength;
+                if (iListCheckBoard[iChecking] == 0)
+                {
+                    if (iListCheckBoard[iChecking - iLength] != 0)
+                    {
+                        pushAble = true;
+                    }
+                }
+                else if (iListCheckBoard[iChecking] == iListCheckBoard[iChecking - iLength])
+                {
+                    pushAble = true;
+                }
+            }
+        }
+        return pushAble;
     }
 
 }
