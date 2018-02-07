d6b2dda1ec744544552782f9091c3482e1be1b36
PuddingPudding <hwsh70119@gmail.com>
2018-02-07 11:50:18 +0800

    新規格

---
+++ Assets/New Folder/BlockData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockData
+{
+    public enum EBlockType //格子種類
+    {
+        Normal, //普通
+        Obstruct, //障礙物(不可越過)
+        None //沒東西(類似障礙物，但可以越過)
+    }
+}
---
+++ Assets/New Folder/ViewControll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewControll : MonoBehaviour
+{
+    [SerializeField] private Transform m_transformInitPosition; //第一個生成格子物件的位子
+    [SerializeField] private GameObject m_gameObjBlockPrefab; //每個格子的遊戲物件原型
+    [SerializeField] private Vector2 m_vec2BlockInterval = new Vector2(60, 60); //每個格子物件間的間隔
+    [SerializeField] private Text m_textScore;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
