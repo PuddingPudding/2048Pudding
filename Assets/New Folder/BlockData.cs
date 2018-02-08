8456e25a2d5cf774b93059f9fd7cd5f314994663
PuddingPudding <hwsh70119@gmail.com>
2018-02-08 13:11:28 +0800

    將新架構的幾個基本零件拼出，還未跟畫面層接上，目前要先測試隨機生成格子

--- Assets/New Folder/BlockData.cs
+++ Assets/New Folder/BlockData.cs
@@ -10,4 +10,44 @@ public class BlockData
         Obstruct, //障礙物(不可越過)
         None //沒東西(類似障礙物，但可以越過)
     }
+    private EBlockType m_eBlockType; //自己的格子種類
+    private int m_iBlockValue; //自己所帶的數值
+
+    public BlockData(EBlockType _eBlockType , int _iBlockValue)
+    {
+        this.m_eBlockType = _eBlockType;
+        this.m_iBlockValue = _iBlockValue;
+    }
+    public BlockData(EBlockType _eBlockType)
+        :this(_eBlockType , 0) //假如只給格子種類的話就預設數值為0
+    {
+    }
+    public BlockData()
+        :this(EBlockType.Normal , 0) //假如什麼都不設定就假設自己是普通格子，且數值為0
+    {
+    }
+    
+    public void PlusOn(BlockData _anotherBlock)
+    {
+        this.m_iBlockValue += _anotherBlock.m_iBlockValue;
+        _anotherBlock.m_iBlockValue = 0;
+    }
+
+    public void SetBlockType(EBlockType _newType)
+    {
+        this.m_eBlockType = _newType;
+    }
+    public EBlockType GetBlockType()
+    {
+        return this.m_eBlockType;
+    }
+    //我感覺如果只是這樣寫的話，那似乎也不用get和set，所以有些猶豫
+    public void SetBlockValue(int _iNewValue)
+    {
+        this.m_iBlockValue = _iNewValue;
+    }
+    public int GetBlockValue()
+    {
+        return this.m_iBlockValue;
+    }
 }
---
+++ Assets/New Folder/GamingLogic2048.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamingLogic2048
+{
+    private List<BlockData> m_listBoard; //整個棋盤
+    private int m_iScore; //分數
+    private int m_iLengthX, m_iLengthY; //X軸邊長，和Y軸邊長
+    private int m_iBoardSize; //整個棋盤的大小
+
+    public GamingLogic2048(int _iLengthX, int _iLengthY, int _iScore) //最完善的建構子
+    {
+        this.m_iLengthX = _iLengthX;
+        this.m_iLengthY = _iLengthY;
+        this.m_iScore = _iScore;
+        this.m_iBoardSize = m_iLengthX * m_iLengthY;
+        this.m_listBoard = new List<BlockData>();
+
+        for (int i = 0; i < m_iBoardSize; i++)
+        {
+            m_listBoard.Add(new BlockData());
+        }
+    }
+    public GamingLogic2048() //最基本的遊戲開局
+        : this(4, 4, 0)
+    {
+    }
+
+    public void SpawnBlock(int _iSpawnPos, BlockData _block) //生成格子
+    {
+        if (this.m_listBoard != null && _iSpawnPos < this.m_iBoardSize)//只在棋盤有被生出來，而且你選定的索引值在範圍內時才允許生成
+        {
+            this.m_listBoard[_iSpawnPos] = _block;
+        }
+    }
+
+    public void SpawnRandomBlock(int _iSpawnNum)
+    {
+        List<BlockData> listEmptyBlock = new List<BlockData>(); //用來記憶哪些格子是空(0)的
+        for (int i = 0; i<m_iBoardSize; i++)
+        {
+            if (m_listBoard[i].GetBlockType() == BlockData.EBlockType.Normal && m_listBoard[i].GetBlockValue() == 0) //只去找普通格子，判斷其是否為0
+            {
+                listEmptyBlock.Add(m_listBoard[i]);
+            }
+        }
+        for (int i = 0; i < _iSpawnNum && listEmptyBlock.Count > 0 ; i++) //listEmptyBlock.Count > 0的用意是要檢查還有沒有空(為0)的格子
+        {
+            int iRandomValue = Random.Range(1, 3) * 2;
+            int iBlockChose = Random.Range(0, listEmptyBlock.Count);
+            listEmptyBlock[iBlockChose].SetBlockValue(iRandomValue);
+            listEmptyBlock.RemoveAt(iBlockChose); //在設定完亂數後，將該選定之格子踢出列表，下一輪亂數就不會再出現了
+        }
+    }
+}
